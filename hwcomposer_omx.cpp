242c23ec6f175afc22e645fb013bb3a007409f7f
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2019-06-11 14:14:16 +0900

    Add License and Copyright notices
    - Replace License description with SPDX-License-Identifier
    - Add Copyright notice
    - Replace name and author description with SNI's
    - Remove README.android
    
    Change-Id: I608ac2ea1a6956dc70039f42ca77143fbaf1176e

--- hwcomposer_omx.cpp
+++ hwcomposer_omx.cpp
@@ -1,18 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
 /*
  * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2015 Socionext Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * based on hardware/libhardware/modules/hwcomposer/hwcomposer.cpp
  */
+
 #define ATRACE_TAG ATRACE_TAG_GRAPHICS
 #include <utils/Trace.h>
 
@@ -104,8 +97,8 @@ hwc_module_t HAL_MODULE_INFO_SYM = {
         .module_api_version = HWC_MODULE_API_VERSION_0_1,
         .hal_api_version    = HARDWARE_HAL_API_VERSION,
         .id = HWC_HARDWARE_MODULE_ID,
-        .name = "Sample hwcomposer module",
-        .author = "The Android Open Source Project",
+        .name = "hwcomposer module for SNI",
+        .author = "Socionext Inc.",
         .methods = &hwc_module_methods,
         .dso = NULL,
         .reserved = {0},
